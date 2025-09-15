09227a2e5998c2f05082ec65c593855e7dcbc20e
DEXMAX009 <dexmax2929@gmail.com>
2025-09-15 20:54:15 +0500

    Задание по программированию
    Делал задания на условия и  print'а

--- Dekes/Main.py
+++ Dekes/Main.py
@@ -39,13 +39,13 @@
 
 # Как не делить
 # 1. Деление на ноль
-# 2. Деление целогоо числа на ноль
+# 2. Деление целого числа на ноль
 # 3. Нахождения остатка от деления на ноль
 #
-# РЕР-8 Руководсто по коду на Python
-# 1) Имена функций, переменнных, классов, должны состоять из маленьких букв, а слово разделяеться
+# РЕР-8 Руководство по коду на Python
+# 1) Имена функций, переменных, классов, должны состоять из маленьких букв, а слово разделяется
 # символами подчёркивания - это необходимо, чтобы их было легче прочитать(fun, my_fun)
-# 2)Нельзя давать имена, которые зареганы в самом Python: int, False, True, None, if, in, from. return, list, float,...
+# 2)Нельзя давать имена, которые зарегистрированы в самом Python: int, False, True, None, if, in, from. return, list, float,...
 
 # my_var = 100
 # my_var = my_var + 200 + 300
@@ -60,6 +60,59 @@
 # result = a - b - c
 # print(result)
 
-a = int(input('Введите число'))
-b = int(input('Введите число'))
-c = int(input('Введите число'))
+#Практика 2
+# Задание 1
+# a = int(input('Введите число'))
+# b = int(input('Введите число'))
+# c = int(input('Введите число'))
+# sum_number = a + b + c
+# pro_number = a * b * c
+# print(sum_number)
+# print(pro_number)
+
+#Задание 2
+# Month = int(input('Введите зп за месяц'))
+# credit = int(input('Введите сумму кредита в банке за месяц'))
+# Communal_services = int(input('Введите задолженность за коммунальные услуги'))
+# raz_number = Month - credit - Communal_services
+# print(raz_number)
+
+#Задание 3
+# d1 = int(input("Введите длину диагонали: "))
+# d2 = int(input("Введите длину диагонали: "))
+# S_Romb = (d1 * d2) / 2
+# print(S_Romb)
+
+#Задание 4
+#print("To be\nor not\nto be")
+
+#Задание 5
+#print("Life is what happens\n                      when\n                            you're busy making other plans\"\n                                                             Jonh Lennon")
+
+#Практика 1
+#Задание 1
+# a = int(input('Введите число'))
+# b = int(input('Введите число'))
+# c = int(input('Введите число'))
+# sum_number = a + b + c
+# pro_number = a * b * c
+# Change = int(input('Выберете действие'))
+# if Change == 1:
+#     print(sum_number)
+# if Change == 2:
+#     print(pro_number)
+
+#Задание 2
+# a = int(input('Введите число'))
+# b = int(input('Введите число'))
+# c = int(input('Введите число'))
+# Arifm = (a + b + c) / 3
+# Change = int(input('Выберете действие'))
+# if Change == 1:
+#     print(max)
+# if Change == 2:
+#     print(min)
+# if Change == 3:
+#     print(Arifm)
+
+#Практика 2
