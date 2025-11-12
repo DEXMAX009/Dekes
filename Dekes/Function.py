916c77a4ffcebc1807362a9a4c26d7d3d0b25921
main <230123872+kirillnelemon@users.noreply.github.com>
2025-11-12 13:03:22 +0500

    exercise

--- Dekes/Function.py
+++ Dekes/Function.py
@@ -1,6 +1,6 @@
-from Main import print_quote, draw_square, print_even_numbers
-
-print_quote()
-print_even_numbers(3, 11)
-draw_square(5, '*', False)
-draw_square(5, '*', True)
+# from Main import print_quote, draw_square, print_even_numbers
+#
+# print_quote()
+# print_even_numbers(3, 11)
+# draw_square(5, '*', False)
+# draw_square(5, '*', True)
